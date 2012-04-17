19733f6fd292472439560656267268cc9ab783b8
Shabinesh Sivaraj <sshabinesh@gmail.com>
2012-04-17 09:15:00 +0530

    a simple until to generate time sortable unique ids

--- id.c
+++ id.c
@@ -3,9 +3,9 @@
 #include<gmp.h>
 int main()
 {
-	struct timeval t;
-	
-	func(16777217);
+	int i;
+	for(i =0;i < 32; i++)
+		func(i);
 	return 0;
 }
 int func(unsigned long u_id)
@@ -14,7 +14,6 @@ int func(unsigned long u_id)
 	unsigned long id;
 	gettimeofday(&t,NULL);
 	id = (t.tv_sec * 1000 * 1000) + (t.tv_usec * 1000) << 42;
-	printf("%lu   ",id);
 	id |= (u_id % 16777216) << 24; 
 	printf("%lu\n ",id);
 	return 0;
