846f51b9379605ba43d5c8d5219163f00aa4a656
Shabinesh <shabi@fossix.org>
2012-04-17 09:37:07 +0530

    Update id.c

--- id.c
+++ id.c
@@ -14,7 +14,7 @@ int func(unsigned long u_id)
 	unsigned long id;
 	gettimeofday(&t,NULL);
 	id = (t.tv_sec * 1000 * 1000) + (t.tv_usec * 1000) << 42;
-	id |= (u_id % 16777216) << 24; 
+	id |= (u_id % 16777216) << 22; 
 	printf("%lu\n ",id);
 	return 0;
 }
